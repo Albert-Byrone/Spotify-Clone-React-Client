9fcb80d5962773ea11826408df58c35ce78865b0
albertbyrone <albertbyrone16777@gmail.com>
2020-12-18 07:58:05 +0300

    use the token for spotify api  authentication

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
     const _token = hash.access_token;
     window.location.hash = ""
     console.log(_token);
+    if(_token){
+      setToken(_token);
+      spotify.setAccessToken(_token);
+    }
   return (
     <div className="app">
       <h1>Spotify-clone</h1>
