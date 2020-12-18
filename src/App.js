37a36e61553a9f8045224226526ad3b23b234e94
albertbyrone <albertbyrone16777@gmail.com>
2020-12-18 07:57:30 +0300

    get the access token from the url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 
 function App() {
+    const hash = getTokenfromUrl();
+    const _token = hash.access_token;
+    window.location.hash = ""
+    console.log(_token);
   return (
     <div className="app">
       <h1>Spotify-clone</h1>
