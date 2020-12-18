e8820dfb211b3c5cd7674b016defcb2bfba9bf10
albertbyrone <albertbyrone16777@gmail.com>
2020-12-18 07:59:06 +0300

    minor imports

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Login from './Login';
+import { getTokenfromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify  = new SpotifyWebApi();
 
 function App() {
+  const [ token, setToken] =  useState(null);
+
+  useEffect(() => {
     const hash = getTokenfromUrl();
     const _token = hash.access_token;
     window.location.hash = ""
     console.log(_token);
+
     if(_token){
       setToken(_token);
       spotify.setAccessToken(_token);
     }
+  }, [])
   return (
     <div className="app">
+      {/* if there is ayoken render the player screen and if  not then renderv the login page */}
       {
         token ? (
           <h1>Logged In</h1>
