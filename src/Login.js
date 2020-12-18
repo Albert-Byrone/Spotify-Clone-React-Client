9c891698d026b30f8475e36d60ef167a96329e26
albertbyrone <albertbyrone16777@gmail.com>
2020-12-18 07:55:30 +0300

    redirect to the spotify login API

--- src/Login.js
+++ src/Login.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import './Login.css';
+import { loginUrl } from './spotify';
+
+function Login() {
+    return (
+        <div className="login">
             <img  src="https://i.ibb.co/3dxhbnY/spotify.png" al="logo" />
+            <a href={ loginUrl}>LOGIN WITH SPOTIFY</a>            
+        </div>
+    )
+}
+
+export default Login
