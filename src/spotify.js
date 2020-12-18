a1041c40cfaf6a3bc01e4146c76e42fe75e48d25
albertbyrone <albertbyrone16777@gmail.com>
2020-12-18 07:54:19 +0300

    export the loginUrl

--- src/spotify.js
+++ src/spotify.js
@@ -9,6 +9,7 @@ const scopes = [
     "user-top-read",
     "user-modify-playback-state",
 ];
+
 export const getTokenfromUrl = () =>{
     return window.location.hash
         .substring(1)
@@ -18,3 +19,5 @@ export const getTokenfromUrl = () =>{
             initial[parts[0]] = decodeURIComponent(parts[1]);
             return initial;
         }, {})
+}
+export const loginUrl = `${authEndPoint}?client_id=${cliendId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
