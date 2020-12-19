13c851f5f60225811ea9a325117932887e126a1e
albertbyrone <albertbyrone16777@gmail.com>
2020-12-19 17:23:14 +0300

    add the spotify logo

---
+++ src/Sidebar.js
@@ -0,0 +1,4 @@
+            <img
+                className="sidebar__logo" 
+                src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="logo"
+            />
