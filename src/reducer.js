1b0ad126e36fc4cc103e740875f9a43980ebb135
albertbyrone <albertbyrone16777@gmail.com>
2020-12-19 17:25:33 +0300

    add initial values

---
+++ src/reducer.js
@@ -0,0 +1,8 @@
+export const initialState = {
+    user:null,
+    playlist: [],
+    playing: false,
+    item: null,
+    token: 'BQBVbSKvNZvB_1ExKFg7NiLgLR8GH2REBumb1V52vOJa0C32iDrEdNnHdKfV5xTBG4Ew4OgzG2B1vDS4ybTDkImjgLTLQbZR_nZ2rUU80QUnts3AD7QVud61TyxjqQY6jh0MIOEzPJ1lOb9wh2n4DxtcjfIhVQScuY3UvnbLhihKPTER',
+};
+
