324bbb61b763451ce3932aa7b562b4db32bfbfea
albertbyrone <albertbyrone16777@gmail.com>
2020-12-19 17:25:53 +0300

    add the reducer function

--- src/reducer.js
+++ src/reducer.js
@@ -6,3 +6,31 @@ export const initialState = {
     token: 'BQBVbSKvNZvB_1ExKFg7NiLgLR8GH2REBumb1V52vOJa0C32iDrEdNnHdKfV5xTBG4Ew4OgzG2B1vDS4ybTDkImjgLTLQbZR_nZ2rUU80QUnts3AD7QVud61TyxjqQY6jh0MIOEzPJ1lOb9wh2n4DxtcjfIhVQScuY3UvnbLhihKPTER',
 };
 
+// set the initial valu to the token for testing
+
+const reducer = (state, action ) => {
+    console.log(action);
+
+    switch(action.type){
+        case "SET_USER":
+            return {
+                ...state,
+                user: action.user
+            };
+        case "SET_TOKEN":
+            return {
+                ...state,
+                token: action.token
+            }
+        case "SET_PLAYLISTS":
+            return {
+                ...state,
+                playlists: action.playlist
+            }
+        default:
+            console.log("this s the state",state)
+            return state;
+    }
+}
+
+export default reducer;
