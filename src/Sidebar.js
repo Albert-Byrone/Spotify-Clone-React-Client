2ab3dc8efb5acc0cfd21963877c6545019845755
albertbyrone <albertbyrone16777@gmail.com>
2020-12-19 17:24:45 +0300

    get the user playlist from the datalayer

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,3 +1,4 @@
+    const [{ playlists }, dispatch] = useDataLayerValue();
             <img
                 className="sidebar__logo" 
                 src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="logo"
