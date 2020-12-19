c42db4f6da07898c344a0c7c72aec3dc808e897f
albertbyrone <albertbyrone16777@gmail.com>
2020-12-19 17:23:44 +0300

    included the sidebar options withj different icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,3 +2,7 @@
                 className="sidebar__logo" 
                 src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="logo"
             />
+            <SidebarOption Icon={ HomeIcon } title="Home"/>
+            <SidebarOption Icon={ SearchIcon } title="Search"/>
+            <SidebarOption Icon={ LibraryMusicIcon } title="Your Library"/>
+
