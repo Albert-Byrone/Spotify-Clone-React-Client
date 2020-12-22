00ac11478f3bd8d8f5a89079028f63430960ce6a
albertbyrone <albertbyrone16777@gmail.com>
2020-12-22 14:24:34 +0300

    add the weekly discover banner

--- src/Body.js
+++ src/Body.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import './Body.css';
 import Header from "./Header"
+import { useDataLayerValue } from './DataLayer';
 
 
 function Body( {spotify }) {
+    const [{ discover_weekly }, dispatch] = useDataLayerValue();
+
     return (
         <div className="body">
             <Header  spotify={spotify}/>
             <div className="body__info">
-                <img src="https://i.ibb.co/3rWqjYy/week.jpg" alt="" />
+                <img src={ discover_weekly?.image[0].url } alt="" />
                 <div className="body__infoText">
                     <strong>PLAYLIST</strong>
                     <h2>Discover Weekly</h2>
-                    <p>Song desxfiption</p>
+                    <p>{ discover_weekly?.description }</p>
                 </div>
             </div>
         </div>
